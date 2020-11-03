4bc7bca3b2a555929f714b62df6f49f078b06322
Ilia Lenskii <il.lenskii@gmail.com>
2020-11-03 03:31:38 +0300

    Fixed

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -1,6 +1,15 @@
 package ru.avalon.java.dev.j10.labs.shapes;
 
-public class Circle implements Point {
+/**
+ * Представление об окружности.
+ * <p>
+ * Окру́жность — замкнутая плоская кривая, которая состоит из
+ * всех точек на плоскости, равноудалённых от заданной точки.
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
+ */
+
+public class Circle implements Shape {
     private float radius = 0F;
 
     public Circle(float Radius) {
--- src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
@@ -1,5 +1,14 @@
 package ru.avalon.java.dev.j10.labs.shapes;
 
+/**
+ * Представление о прямоугольнике.
+ * <p>
+ * Прямоугольник — четырехугольник, у которого все углы
+ * прямые (равны 90 градусам).
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Прямоугольник</a>
+ */
+
 public class Rectangle implements Polygon {
     private float side = 0F;
     private float side2 = 0F;
